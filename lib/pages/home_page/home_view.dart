1259beca5d2b22134bca59c6eeaf4850385f517f
Vini Porto <vinicius.p.a.corin@gmail.com>
2023-12-08 23:16:08 -0300

    corrigindo favorite icons e função de download

--- lib/pages/home_page/home_view.dart
+++ lib/pages/home_page/home_view.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart';
 import 'package:escribo_ebook/models/ebook_model.dart';
 import 'package:escribo_ebook/pages/home_page/home_controller.dart';
@@ -8,6 +10,10 @@ import 'package:flutter/material.dart';
 
 import 'package:escribo_ebook/utils/escribo_colors.dart';
 import 'package:escribo_ebook/pages/home_page/components/book_box_widget.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
+import 'package:flutter_file_downloader/flutter_file_downloader.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:vocsy_epub_viewer/epub_viewer.dart';
 
 import 'components/book_toggle_widget.dart';
 
@@ -23,6 +29,68 @@ class _HomePageState extends State<HomePage> {
   void initState() {
     super.initState();
     controller.getBooks();
+  }
+
+  openBook(path, location) async {
+    VocsyEpub.setConfig(
+      themeColor: Theme.of(context).primaryColor,
+      identifier: "iosBook",
+      scrollDirection: EpubScrollDirection.ALLDIRECTIONS,
+      allowSharing: true,
+      enableTts: true,
+      nightMode: true,
+    );
+    // get current locator
+    VocsyEpub.locatorStream.listen((locator) {
+      print('LOCATOR: $locator');
+    });
+    await VocsyEpub.openAsset(
+      path,
+      lastLocation: EpubLocator.fromJson(location),
+      /*
+      {
+        "bookId": "http://www.gutenberg.org/72134",
+        "href": "/OEBPS/5921883067179669558_72134-h-0.htm.xhtml",
+        "created": 1702061715408,
+        "locations": {"cfi":"epubcfi(/0!/4/6/6/2[pgepubid00000])"},
+        "title": ""
+      }
+      */
+    );
+  }
+
+  downloadBook(BookModel book) async {
+    // FileDownloader.downloadFile(
+    //     url: book.downloadUrl,
+    //     name: book.title,
+    //     notificationType: NotificationType.progressOnly,
+    //     onDownloadCompleted: (String path) {
+    //       path = 'assets/epubs';
+
+    //       print('FILE DOWNLOADED TO PATH: $path');
+    //     });
+
+    // Directory? directory = await getExternalStorageDirectory();
+
+    // if (directory != null) {
+    //   String baseDirPath = '${directory.path}/assets/epubs';
+    //   print(baseDirPath);
+
+   
+    //   // File file = File('$baseDirPath/$fileName');
+
+    //   final dio = Dio();
+    //   dio.download(book.downloadUrl, baseDirPath);
+    //   book.isDownloaded = true;
+    // }
+    final bookId = await FlutterDownloader.enqueue(
+      url: book.downloadUrl,
+      savedDir: 'assets/epubs',
+      showNotification:
+          true, 
+      openFileFromNotification:
+          true, 
+    );
   }
 
   final controller = HomeController(
@@ -86,6 +154,28 @@ class _HomePageState extends State<HomePage> {
                               (index) => BookBoxWidget(
                                 title: bookList[index].title,
                                 bookCover: bookList[index].coverUrl,
+                                isFavorited: bookList[index].isFavorited,
+                                isDownloaded: bookList[index].isDownloaded,
+                                onTapCover: () async => {
+                                  setState(() {
+                                    (bookList[index].isDownloaded)
+                                        ? openBook("", "")
+                                        : downloadBook(
+                                            bookList[index],
+                                          );
+                                  })
+                                },
+                                onTapFavorite: () {
+                                  if (bookList[index].isFavorited) {
+                                    setState(() {
+                                      bookList[index].unFavorite();
+                                    });
+                                  } else {
+                                    setState(() {
+                                      bookList[index].favorite();
+                                    });
+                                  }
+                                },
                               ),
                             ),
                           ),
